41a23f29143584c985bfab7c77b6fb0c1139b0ba
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-03-12 16:04:29 -0300

    Tipos de seletores: Classe, Parametro e Padrão (tag)

--- app1/src/app/topo/topo.component.ts
+++ app1/src/app/topo/topo.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-topo',
+    selector: 'app-topo', //pode ser feito como tag (sem nada) - Como parametro passando usando [] - Como classe colocando um . antes do nome
     templateUrl: './topo.component.html',
     //template : '<p> Esse é o componente topo</p>' //Com aspas simples ' é pra ficar tudo em uma linha
     /*template: `<p> //Já com crase, pode quebrar linha;
