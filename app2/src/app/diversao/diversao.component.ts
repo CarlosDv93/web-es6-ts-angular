c8880fe9fc71865a3ff53251a9cd9bb0b35248af
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-03-21 10:29:16 -0300

    Recuperando Ofertas de diversão e ajustanto o HTML

--- app2/src/app/diversao/diversao.component.ts
+++ app2/src/app/diversao/diversao.component.ts
@@ -1,15 +1,24 @@
+import { OfertasService } from './../ofertas.service';
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app2-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [ OfertasService ]
 })
 export class DiversaoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas : Oferta[];
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertasService.getOfertasPorCategoria('diversao')
+      .then((ofertasResponse: Oferta[]) => {
+        this.ofertas = ofertasResponse;
+      })
   }
 
 }
