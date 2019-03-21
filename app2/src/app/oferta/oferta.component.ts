628e3896a926558ac7ff4a845fdb4eabfae1a0a8
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-03-21 15:57:30 -0300

    Atualizando o template de oferta e alterando a maneira como a URL é acessada

--- app2/src/app/oferta/oferta.component.ts
+++ app2/src/app/oferta/oferta.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app2-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  private oferta: Oferta;
+
+  constructor(private route: ActivatedRoute,
+      private ofertaService: OfertasService) { }
 
   ngOnInit() {
-    /*Usando SnapShot para buscar parametro
-    this.route.snapshot.params['id'];*/
+    //Usando SnapShot para buscar parametro
+    this.ofertaService.getOfertaPorId(this.route.snapshot.params['id'])
+      .then((ofertaResponse: any) => this.oferta = ofertaResponse);
 
-    //Usando subscribe para buscar parametro
+    /*//Usando subscribe para buscar parametro
     this.route.params.subscribe((parametro: any) => {
       console.log(parametro);
-    })
+    })*/
     
   }
 
--- app2/src/app/ofertas.service.ts
+++ app2/src/app/ofertas.service.ts
@@ -8,21 +8,29 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class OfertasService {
 
+    private url_api = 'http://localhost:3000/ofertas';
+
     constructor(private httpClientModule : HttpClientModule,
         private http : HttpClient){
 
     }
     
     public getOfertas(): Promise<Oferta[]> {
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${this.url_api}?destaque=true`)
         .toPromise()
         .then( (ofertasResposta : any) => ofertasResposta);
     }
 
     public getOfertasPorCategoria(categoria:string) : Promise<Array<Oferta>> {
         //pode concatenar as string ou usar o backtip (` - crase) e usar o binding
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${this.url_api}?categoria=${categoria}`)
         .toPromise()
         .then((ofertasRestaurante : any) => ofertasRestaurante);
+    }
+
+    public getOfertaPorId(id:number): Promise<Oferta> {
+        return this.http.get(`${this.url_api}?id=${id}`)
+            .toPromise()
+            .then((ofertaId: any) => ofertaId[0]);
     }
 }
