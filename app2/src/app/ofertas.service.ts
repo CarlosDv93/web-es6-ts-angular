bb1ab7cb4de00a0d28d47e54b2048b462815509e
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-03-21 16:22:11 -0300

    Alterando a URL para constante externa

--- app2/src/app/ofertas.service.ts
+++ app2/src/app/ofertas.service.ts
@@ -2,13 +2,15 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Oferta } from './shared/oferta.model';
 import { Injectable } from '@angular/core';
 
+import { URL_API } from './app.api';
+
 //Import do toPromise
 //import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class OfertasService {
 
-    private url_api = 'http://localhost:3000/ofertas';
+    //private url_api = 'http://localhost:3000/ofertas';
 
     constructor(private httpClientModule : HttpClientModule,
         private http : HttpClient){
@@ -16,20 +18,20 @@ export class OfertasService {
     }
     
     public getOfertas(): Promise<Oferta[]> {
-        return this.http.get(`${this.url_api}?destaque=true`)
+        return this.http.get(`${URL_API}?destaque=true`)
         .toPromise()
         .then( (ofertasResposta : any) => ofertasResposta);
     }
 
     public getOfertasPorCategoria(categoria:string) : Promise<Array<Oferta>> {
         //pode concatenar as string ou usar o backtip (` - crase) e usar o binding
-        return this.http.get(`${this.url_api}?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
         .toPromise()
         .then((ofertasRestaurante : any) => ofertasRestaurante);
     }
 
     public getOfertaPorId(id:number): Promise<Oferta> {
-        return this.http.get(`${this.url_api}?id=${id}`)
+        return this.http.get(`${URL_API}?id=${id}`)
             .toPromise()
             .then((ofertaId: any) => ofertaId[0]);
     }
