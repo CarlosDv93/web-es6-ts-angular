4562e5742a172d2ee8e4511951238dec147e9ad6
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-03-26 11:30:21 -0300

    Adicionando campo de pesquisa e retirada dos exemplos dos observables

--- app2/src/app/oferta/oferta.component.ts
+++ app2/src/app/oferta/oferta.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
-import { Observable, Subscription } from 'rxjs';
-import { Observer } from 'rxjs/';
 import 'rxjs/Rx';
 
 @Component({
@@ -14,9 +12,6 @@ import 'rxjs/Rx';
 })
 export class OfertaComponent implements OnInit, OnDestroy {
 
-  private tempoObservableSubscription: Subscription;
-  private meuObservableTesteSubscription: Subscription;
-
   public oferta: Oferta;
 
   constructor(private route: ActivatedRoute,
@@ -26,46 +21,9 @@ export class OfertaComponent implements OnInit, OnDestroy {
     //Usando SnapShot para buscar parametro
     this.ofertaService.getOfertaPorId(this.route.snapshot.params['id'])
       .then((ofertaResponse: any) => this.oferta = ofertaResponse);
-
-    /*//Usando subscribe para buscar parametro
-    this.route.params.subscribe((parametro: any) => {
-      console.log(parametro);
-    })*/
-
-    /*
-    // Exemplo de Observable
-    this.route.params.subscribe(
-      (parametros: any) => {console.log(parametros); }, //Quando dá certo
-      (error: any) => { console.log(error)}, //Quando tem algum erro
-      () => { console.log("Processamento foi Classificado como Concluido")} //Quando está concluido
-    )
-    */
-
-    let tempo = Observable.interval(2000);
-    this.tempoObservableSubscription = tempo.subscribe(arg => console.log(arg));
-    
-
-    //observable (observável)
-    let observavel = Observable.create((observer: Observer<string>) => {
-      observer.next('Primeiro evento da stream');
-      observer.next('Segundo evento da stream');
-      //observer.complete();
-      observer.next('Terceiro evento da stream');
-      observer.error('Algum erro foi encontrado na stream de eventos');
-      observer.complete();
-    })
-
-    //observable (observador - ao se dar subscribe, vira o observable)
-    this.meuObservableTesteSubscription= observavel.subscribe(
-      (resultado : any) => { console.log(resultado)},
-      (error: any) => { console.log(error)},
-      () => { console.log('Evento Completo')}
-    );
   }
 
   ngOnDestroy() {
-    this.meuObservableTesteSubscription.unsubscribe();
-    this.tempoObservableSubscription.unsubscribe();
   }
 
 }
