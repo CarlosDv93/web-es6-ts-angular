c17217ba528ffd4fb714dec5d084c754647f9ebe
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-06-17 17:18:11 -0300

    Fazendo pipe personalizado

--- app2/src/app/util/descricao-reduzida.pipe.ts
+++ app2/src/app/util/descricao-reduzida.pipe.ts
@@ -5,11 +5,21 @@ import { PipeTransform, Pipe } from '@angular/core';
 })
 export class DescricaoReduzida implements PipeTransform {
 
-    transform(texto: string) : string {
-        if(texto.length >= 15) {
-            return texto.substr(0, 15) + '...';
+    transform(texto: string, truncarEm: number) : string {
+        if(texto.length >= truncarEm) {
+            return texto.substr(0, truncarEm) + '...';
         } else {
             return texto;
         }
     }
+
+    /* //3 Parametros --> Uso: nomePipe:param1:param2 --> descricaoReduzida:15:2
+        transform(texto: string, truncarEm: number, iniciarEm: number) : string {
+        if(texto.length >= truncarEm) {
+            return texto.substr(iniciarEm, truncarEm) + '...';
+        } else {
+            return texto;
+        }
+    }
+    */
 }
