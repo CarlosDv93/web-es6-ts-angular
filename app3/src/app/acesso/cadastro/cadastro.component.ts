780218666f98c44a961ff167d4711e5d1841e404
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-07-25 17:47:54 -0300

    FormGroup v1 - WIP

--- app3/src/app/acesso/cadastro/cadastro.component.ts
+++ app3/src/app/acesso/cadastro/cadastro.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
+  providers: [ FormGroup, FormControl ]
 })
 export class CadastroComponent implements OnInit {
 
   @Output() public exibirPainel : EventEmitter<string> = new EventEmitter<string>();
+
+  public formularioCadastro: FormGroup = new FormGroup({
+    'email': new FormControl(null),
+    'nomeCompleto': new FormControl(null),
+    'usuario': new FormControl(null),
+    'senha': new FormControl(null)
+  })
 
   constructor() { }
 
@@ -17,6 +26,10 @@ export class CadastroComponent implements OnInit {
 
   public exibePainelLogin(event) : void {
     this.exibirPainel.emit('login');
+  }
+
+  public cadastrarUsuario() {
+    console.log(this.formularioCadastro);
   }
 
 }
