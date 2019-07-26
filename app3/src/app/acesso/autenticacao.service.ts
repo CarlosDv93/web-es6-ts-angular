23f07f2ba241b30d8eecc51a0f7218d3a0608917
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-07-26 16:58:15 -0300

    Autenticação - Tentando fazer conexão com o banco de dados - WIP

--- app3/src/app/acesso/autenticacao.service.ts
+++ app3/src/app/acesso/autenticacao.service.ts
@@ -1,8 +1,29 @@
 import { Usuario } from './usuario.model';
+import * as firebase from 'firebase'
 
 export class Autenticacao {
     public cadastrarUsuario(usuario: Usuario) : void {
         console.log("Chegamos até o serviço com o param: " , usuario);
-        
+
+        firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
+            .then((resposta: any) => {
+
+                //remove a senha do atributo senha
+                delete usuario.senha;
+
+                //registra dados complementares do usuario no path email na base64
+                //btoa -> Critptografa
+                //atob -> descriptografa
+                let emailCripto = btoa(usuario.email);
+                console.log(emailCripto);
+
+                let database = firebase.database();
+                console.log("Database: ", database);
+                firebase.database().ref(`usuario_detalhe/${emailCripto}`)
+                    .set( usuario );
+            })
+            .catch((erro: Error) => {
+                console.log(erro);
+            })
     }
 }
--- app3/src/app/app.component.ts
+++ app3/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app3';
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    var firebaseConfig = {
+      apiKey: "AIzaSyBWQJM9vqvW5zgH8l6WxOIH_mrA6ewdNbw",
+      authDomain: "carlosdv93-jta-cloneinsta.firebaseapp.com",
+      databaseURL: "https://carlosdv93-jta-cloneinsta.firebaseio.com",
+      projectId: "carlosdv93-jta-cloneinsta",
+      storageBucket: "",
+      messagingSenderId: "360076863823",
+      appId: "1:360076863823:web:38b23bc3ab438138"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+    // Get a reference to the database service
+    var database = firebase.database();
+    console.log("DATABASE INICIADO: ", database);
+
+  }
 }
