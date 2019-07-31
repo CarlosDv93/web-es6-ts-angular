93a8da3bb967c924a683761cc39d4afd2a591268
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-07-31 11:21:27 -0300

    ajustando nome da imagem / publicacao para serem o mesmo

--- app3/src/app/bd.service.ts
+++ app3/src/app/bd.service.ts
@@ -6,36 +6,36 @@ import { Progresso } from './progresso.service';
 @Injectable()
 export class BD {
 
-    constructor(private progresso : Progresso) { }
+    constructor(private progresso: Progresso) { }
 
-    public publicar(publicacao: any) : void {
+    public publicar(publicacao: any): void {
 
         console.log(publicacao);
 
-        let nomeImagem = Date.now();
+        let nomeImagem;
 
-        firebase.storage().ref()
-            .child(`imagens/${nomeImagem}`)
-            .put(publicacao.imagem)
-            .on(firebase.storage.TaskEvent.STATE_CHANGED,
-                (snapshot: any) => {
-                    this.progresso.status = 'andamento';
-                    this.progresso.estado = snapshot;
-                    // console.log("Spanshot: ", snapshot);
-                },
-                (error: Error) => {
-                    this.progresso.status = 'erro';
-                    // console.log(error);
-                },
-                () => { //Finalização do Processo
-                    this.progresso.status = 'concluido';
-                    // console.log("Upload Completo")
-                }
-                );
+        firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
+            .push({ titulo: publicacao.titulo })
+            .then((resposta: any) => {
+                nomeImagem = resposta.key;
 
-        /* firebase.database().ref(`/publicacoes/${btoa(publicacao.email)}`)
-            .push({ titulo: publicacao.titulo}); */
+                firebase.storage().ref()
+                    .child(`imagens/${nomeImagem}`)
+                    .put(publicacao.imagem)
+                    .on(firebase.storage.TaskEvent.STATE_CHANGED,
+                        (snapshot: any) => {
+                            this.progresso.status = 'andamento';
+                            this.progresso.estado = snapshot;
+                        },
+                        (error: Error) => {
+                            this.progresso.status = 'erro';
+                        },
+                        () => { //Finalização do Processo
+                            this.progresso.status = 'concluido';
 
-        console.log("Chegamos até o serviço responsável pelo controle de dados")
+                        }
+                    );
+            })
+
     }
 }
